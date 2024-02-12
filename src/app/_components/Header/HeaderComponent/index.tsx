c1590f5c2793f52afdc2ec762b650190a56fc9c3
Cody McEachern <113365765+cmacspeedo@users.noreply.github.com>
2024-02-12 16:00:54 -0700

    new styles and admin work

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client'
 
-const index = () => {
+import React, { useCallback } from 'react'
+import Link from 'next/link'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+import Image from 'next/image'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
   return (
-    <div>index</div>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
   )
 }
 
-export default index
+export default HeaderComponent
