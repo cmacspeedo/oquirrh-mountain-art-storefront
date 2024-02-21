85dd2e367b2fc44795b5df45e6e58904d9efb82f
Cody McEachern <113365765+cmacspeedo@users.noreply.github.com>
2024-02-21 13:40:52 -0700

    categories and promotions

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 
 import { Category } from '../../../../payload/payload-types'
-// import { useFilter } from '../../../_providers/Filter'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
