04ab7ad4f0d51f6c1f6147ae89a723d455cab49f
Cody McEachern <113365765+cmacspeedo@users.noreply.github.com>
2024-02-20 18:32:43 -0700

    footer work

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,26 +1,40 @@
+'use client'
+
 import React from 'react'
 
-import classes from '.index.module.scss'
+import classes from './index.module.scss'
 import { Footer } from '..'
-import { noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { constants } from 'buffer'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
+import Image from 'next/image'
 
 const FooterComponent = () => {
   const pathname = usePathname()
 
   return (
-    <footer className={noHeaderFooterUrls.includes(pathname) ?
-    classes.hide : ''}>
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
       <Gutter>
-        <ul className={classes.inclusions}></ul>
+        <ul className={classes.inclusions}>
+          {inclusions.map(inclusion => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
 
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
       </Gutter>
-
     </footer>
   )
-
 }
 
 export default FooterComponent
